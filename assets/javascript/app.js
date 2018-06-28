c50a0dd1266f7e31d92424ea7b4538821b999636
Stephen Pellegrini <spellegrini156@gmail.com>
2018-06-28 10:24:43 -0700

    file structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+var apiKey = 'afbbed97e0fc4da5ba0fd364a1866e4a';
+
+function getSearchTerms() {
+    //check search term
+    var searchTerm = $('.search-term').val();
+    var numRecords = $('.search-term').val();
+    var startYear = $('.start-year').val();
+    var endYear = $('.end-year').val();
+}
+
+
+
