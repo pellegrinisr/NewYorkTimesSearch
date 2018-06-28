ea5ed8391542f77d114031af8e1d7895b808ac43
Stephen Pellegrini <spellegrini156@gmail.com>
2018-06-28 12:09:07 -0700

    getJSON call working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,44 @@
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-var apiKey = 'afbbed97e0fc4da5ba0fd364a1866e4a';
+$(document).ready(function() {
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    var apiKey = 'api_key=afbbed97e0fc4da5ba0fd364a1866e4a';
+    var serarchTerm;
+    var numRecords;
+    function getSearchTerms() {
+        // console.log('.search-term' + $('.search-term').val());
+        // //check search term
+        // if ($('.search-term').val() !== '') {
+            
+        //     serarchTerm = $('.search-term').val();
+        //     numRecords = $('.num-records').val();
+            
+        // } else {
+        //     alert('Search term and Number of Records')
+        // }
+        // console.log('search term: ' + serarchTerm);
+        // console.log('number of records: ' + numRecords);
+        // $.getJASON()
 
-function getSearchTerms() {
-    //check search term
-    var searchTerm = $('.search-term').val();
-    var numRecords = $('.search-term').val();
-    var startYear = $('.start-year').val();
-    var endYear = $('.end-year').val();
-}
+        console.log($('#searchTerm').val());
+        console.log($('#numRecordsSelect').val());   
+
+        searchTerm = $('#searchTerm').val();
+        numRecords = $('#numRecordsSelect').val();
+        console.log(searchTerm + '   ' + numRecords);
+
+        var queryURL = url + '?' + apiKey + '&q=' + searchTerm;
+        console.log(queryURL);
+
+        $.getJSON(queryURL).then(function(response) {
+            //create new div to hold each article result
+            console.log(response);
+        })
+    }
+
+    $(document).on('click', '.search-button', getSearchTerms);
+});
+
+
+
 
 
 
