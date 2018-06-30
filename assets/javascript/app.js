27f5f926b79c4b79588f67752513be29b4b39324
Stephen Pellegrini <spellegrini156@gmail.com>
2018-06-29 17:48:18 -0700

    img returned in search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var apiKey = 'api_key=afbbed97e0fc4da5ba0fd364a1866e4a';
     var searchTerm;
     var numRecords;
+    
     function getSearchTerms() {
         if ($('#searchTerm').val()) {
             console.log($('#searchTerm').val());
@@ -12,19 +13,26 @@ $(document).ready(function() {
             numRecords = $('#numRecordsSelect').val();
             console.log(searchTerm + '   ' + numRecords);
     
-            var queryURL = url + '?' + apiKey + '&q=' + searchTerm;
+            var queryURL = url + '?' + apiKey + '&q=' + searchTerm + '&page=2';
             console.log(queryURL);
     
             $.getJSON(queryURL).then(function(response) {
                 console.log(response);
                 if (response.status === 'OK') {
                     var numIterations;
+                    var numPages;
+                    
+                    if ($('#numRecordsSelect').val() / response.response.docs.length > 1) {
+                        numPages = 2;
+                    } else {
+                        numPages = 1;
+                    }
                     if (numRecords > response.response.docs.length) {
                         numIterations = response.response.docs.length;
                     } else {
                         numIterations = numRecords;
                     }
-                    for (var i = 0; i < numRecords; i++) {
+                    for (var i = 0; i < numIterations; i++) {
                         //create an empty div to hold new article
                         //and assign it class article-addition
                         var newArticleDiv = $('<div>').addClass('article-addition');
@@ -35,6 +43,13 @@ $(document).ready(function() {
                         headlineP.html(response.response.docs[i].headline.main);
                         //append the headline p to the new article div
                         newArticleDiv.append(headlineP);
+                        //create an empty image tag
+                        //assign it a class
+                        //change its src attribute
+                        //append it to the newArticleDiv
+                        var articlePic = $('<img>').addClass('thumbnail');
+                        articlePic.attr('src', 'https://www.nytimes.com/' + response.response.docs[i].multimedia[3].url);
+                        newArticleDiv.append(articlePic);
                         //verify that result has a byline tag
                         if (response.response.docs[i].byline) {
                             //create p tag for byline
@@ -61,13 +76,14 @@ $(document).ready(function() {
         else {
             alert('Search Terms cannot be blank');
         }
-    }
+    };
+
+    // function checkPagination() {
+    //     numRecords = $('#numRecordsSelect').val();
+    //     var numPages = numRecords / 10;
+
+    // };
+
 
     $(document).on('click', '.search-button', getSearchTerms);
 });
-
-
-
-
-
-
